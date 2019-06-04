4560b33ac2065872b548980179be00eedaad8b9a
Your Name <user@MacBook-Pro-de-Daniela.local>
2019-06-04 04:31:05 -0600

    adding functionaloty

---
+++ app.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById("btn1");
+btn.addEventListener("click", function () {
+    let inputTextType = document.getElementById("valorPrimero").value;
+    let regex_numeros = /^[0-9]+$/;
+    if (regex_numeros.exec(inputTextType)) {
+        // calcular formula
+        let formulaLibrasKilogramos = inputTextType / 2.2046;
+        let show = document.getElementById("resultLibrasGramos").innerHTML = "<h3>" + Math.round(formulaLibrasKilogramos) + "kg " + "</h3>";
+
+    } else {
+        // agregar un return con html
+        let newElement = document.getElementById("resultLibrasGramos").innerHTML = '<p style="display:block;">'+'Por favor,introducir un numero entero!'+'</p>';
+        
+         
+    
+
+    }
+
+}
+);  
