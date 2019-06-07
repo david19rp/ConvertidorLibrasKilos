302860b51e66a3b17802d0637de5c40b8bc71c52
Your Name <user@MacBook-Pro-de-Daniela.local>
2019-06-06 22:47:42 -0600

    adding styles

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// libras a kilos
 let btn = document.getElementById("btn1");
 btn.addEventListener("click", function () {
     let inputTextType = document.getElementById("valorPrimero").value;
@@ -16,5 +17,38 @@ btn.addEventListener("click", function () {
 
     }
 
+
+
+    
+
 }
 );  
+
+// kilometros a millas
+
+
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function () {
+    let inputTextType = document.getElementById("valorPrimero2").value;
+    let regex_numeros = /^[0-9]+$/;
+    if (regex_numeros.exec(inputTextType)) {
+        // calcular formula
+        let formulakilometrosMillas = inputTextType * 0.62;
+        let show = document.getElementById("resultKilometrosMillas").innerHTML = "<h3>" + Math.round(formulakilometrosMillas) + "Millas " + "</h3>";
+
+    } else {
+        // agregar un return con html
+        let newElement = document.getElementById("resultKilometrosMillas").innerHTML = '<p style="display:block;">'+'Introducir un numero entero!'+'</p>';
+        
+         
+    
+
+    }
+
+
+
+    
+
+}
+);  
+
